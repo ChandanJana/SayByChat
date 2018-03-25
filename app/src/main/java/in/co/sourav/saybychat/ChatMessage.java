c603c6b3579742540f079439d244a796d4f5c913
ChandanJana <chandanjana@gmail.com>
2018-03-25 23:56:44 +0530

    Initial commit

---
+++ app/src/main/java/in/co/sourav/saybychat/ChatMessage.java
@@ -0,0 +1,49 @@
+package in.co.sourav.saybychat;
+
+import java.util.Date;
+
+/**
+ * Created by Sourav on 19-Feb-17.
+ */
+
+public class ChatMessage {
+    private String messageText;
+    private String messageUser;
+    private long messageTime;
+
+    public ChatMessage(String messageText, String messageUser) {
+        this.messageText = messageText;
+        this.messageUser = messageUser;
+
+        messageTime = new Date().getTime();
+    }
+
+    public ChatMessage() {
+    }
+
+    public String getMessageText() {
+        return messageText;
+    }
+
+    public void setMessageText(String messageText) {
+        this.messageText = messageText;
+    }
+
+    public String getMessageUser() {
+        return messageUser;
+    }
+
+    public void setMessageUser(String messageUser) {
+        this.messageUser = messageUser;
+    }
+
+    public long getMessageTime() {
+        return messageTime;
+    }
+
+    public void setMessageTime(long messageTime) {
+        this.messageTime = messageTime;
+    }
+
+
+}
